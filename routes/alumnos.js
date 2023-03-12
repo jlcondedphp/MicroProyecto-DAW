860e1f5a37dd39002da87a9f63568517d8d45190
jlcondedphp <joseluis.condediez@gmail.com>
2023-03-12 22:50:54 +0100

    Se borrar archivos

--- routes/alumnos.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const alumnoController = require('../controllers/alumnoController')
-
-router.get('/', alumnoController.mostrar)
-
-module.exports = router
